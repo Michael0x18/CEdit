fbcc6d2b99b91f45eb4f22cb9a250e7d08c6fd9e
Michael <michaelsunn101@gmail.com>
2021-10-08 19:11:50 -0700

    Start Rewrite

---
+++ src/initialize.h
@@ -0,0 +1,8 @@
+#ifndef initialize_h
+#define initialize_h
+
+void initialize(struct estate *state)
+{
+}
+
+#endif
---
+++ src/main.h
@@ -0,0 +1,27 @@
+#ifndef main_h
+#define main_h
+/*
+ * FILE main.h
+ */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+#include <fileioc.h>
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <graphx.h>
+#include <keypadc.h>
+#include <fontlibc.h>
+
+/* 
+ * Main function
+ * Does basic starting
+ */
+int main(int argc, char **argv);
+
+#endif
