c2d463b1c0632fb071280b09f63baeab7c249b37
Michael0x18 <michaelsunn101@gmail.com>
2021-10-20 19:56:02 -0700

    REEEE

--- src/main.c
+++ src/main.c
@@ -2,4 +2,10 @@
 
 int main(int argc, char **argv)
 {
+    if(argc<2){
+        os_ClrHome();
+        os_PutStrFull("Usage: prgmCEDIT varname");
+        while(!os_GetCSC());
+        return 0;
+    }
 }
--- src/state.h
+++ src/state.h
@@ -28,13 +28,8 @@
 
 struct estate
 {
-#ifdef BOS_BUILD
-    //The text buffer (BOS VERSION)
-    char text[65536];
-#else
-    //The text buffer (TIOS VERSION)
-    char text[16384];
-#endif
+    //Unified between the TIOS and BOS versions. Set this to the second half of vram
+    char *text;
     //Array of the lengths of the lines
     char line_lengths[10000];
     //Points to the spot where the cursor is at, on the left side
@@ -49,6 +44,20 @@ struct estate
     int24_t cursor_x;
     //The y position of the cursor, on the screen
     int24_t cursor_y;
+    //The color of text in the foreground
+    uint8_t text_color;
+    //The color of the back of the window
+    uint8_t background_color;
+    //The color of the text that is selected
+    uint8_t text_selection_color;
+    //The color used as a background when highlighting text
+    uint8_t text_selection_highlight_color;
+    //The color of the status bars at the top and bottom
+    uint8_t statusbar_color;
+    //The color of the text on the status bars
+    uint8_t statusbar_text_color;
+    //Just set this to something totally random
+    uint8_t transparent_color;
 };
 
 #endif
