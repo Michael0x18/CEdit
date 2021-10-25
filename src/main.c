d3db449edca5ded26fe3538b0a60265ac7e7330d
Michael0x18 <michael@jacobly.com>
2021-10-25 22:19:05 +0000

    Get blitting and screen setting routines to work
    YAY

--- src/main.c
+++ src/main.c
@@ -7,22 +7,20 @@ int main(int argc, char** argv){
     x4_Begin();
     x4_LoadDefaultPalette();
     x4_SetDrawLocation(X4_BUFFER_1);
-    x4_SetScreenLocation(X4_BUFFER_1);
+    for(int8_t i = 0; i < 16; i++){
+        x4_FillScreen(i);
+        while(!os_GetCSC());
+    }
+    x4_SetDrawLocation(X4_BUFFER_2);
+    x4_FillScreen(7);
+    x4_SetDrawLocation(X4_BUFFER_3);
+    x4_FillScreen(6);
+    //while(!os_GetCSC());
+    x4_BlitBuffer(X4_BUFFER_1,X4_BUFFER_2);
     while(!os_GetCSC());
-    //for(int8_t i = 0; i < 16; i++){
-    //    x4_FillScreen(i);
-    //    while(!os_GetCSC());
-   // }
-   // x4_SetDrawLocation(X4_BUFFER_2);
-    //x4_FillScreen(7);
-    //x4_SetDrawLocation(X4_BUFFER_3);
-    //x4_FillScreen(6);
-    //while(!os_GetCSC());
-    //x4_BlitBuffer(X4_BUFFER_1,X4_BUFFER_2);
-    //while(!os_GetCSC());
-    ////x4_BlitBuffer(X4_BUFFER_1,X4_BUFFER_3);
-    //x4_SetScreenLocation(X4_BUFFER_3);
-    //while(!os_GetCSC());
-    //x4_SetScreenLocation(X4_BUFFER_1);
+    //x4_BlitBuffer(X4_BUFFER_1,X4_BUFFER_3);
+    x4_SetScreenLocation(X4_BUFFER_3);
+    while(!os_GetCSC());
+    x4_SetScreenLocation(X4_BUFFER_1);
     x4_End();
 }
