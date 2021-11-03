278056988867ff7a7505aaefad552fc4f7d8584b
Michael0x18 <michael@jacobly.com>
2021-11-03 22:47:34 +0000

    REE?

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "libx4.h"
 
 int main(void){
+
 	int8_t frame = 0;
 	x4_Begin();
 	x4_LoadDefaultPalette();
@@ -14,11 +15,14 @@ int main(void){
 	x4_FillScreen(15);
 	int cursor_x = 0;
 	int cursor_y = 0;
+
 	while(!kb_IsDown(kb_KeyClear)){
 		x4_FillScreen(15);
-		x4_PutPixel(120,0,0);
-		x4_FastLine_Fix(160,120,cursor_x,cursor_y,0);
+		x4_PutPixel(120, 0, 0);
+
+		x4_FastLine_Fix(160, 120, cursor_x, cursor_y, 0);
 		kb_Scan();
+
 		if(kb_IsDown(kb_KeyLeft))
 			cursor_x--;
 		if(kb_IsDown(kb_KeyRight))
@@ -27,24 +31,32 @@ int main(void){
 			cursor_y--;
 		if(kb_IsDown(kb_KeyDown))
 			cursor_y++;
-		if(cursor_x<0)
-			cursor_x=0;
-		if(cursor_x>319)
-			cursor_x=319;
-		if(cursor_y<0)
-			cursor_y=0;
-		if(cursor_y>239)
-			cursor_y=239;
+
+		if(cursor_x < 0)
+			cursor_x = 0;
+		if(cursor_x > 319)
+			cursor_x = 319;
+		if(cursor_y < 0)
+			cursor_y = 0;
+		if(cursor_y > 239)
+			cursor_y = 239;
+
 		if(frame){
-			frame=0;
+
+			frame = 0;
 			x4_SetScreenLocation(X4_BUFFER_2);
 			x4_SetDrawLocation(X4_BUFFER_1);
+
 		}else{
-			frame=1;
+
+			frame = 1;
 			x4_SetScreenLocation(X4_BUFFER_1);
 			x4_SetDrawLocation(X4_BUFFER_2);
+
 		}
+
 	}
+
 	x4_SetScreenLocation(X4_BUFFER_1);
 	x4_End();
 }
