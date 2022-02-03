474da4387a79e0ddc08afa0b5c0680d055daee6b
Michael0x18 <michaelsunn101@gmail.com>
2022-02-03 10:26:14 -0800

    ree

--- src/main.c
+++ src/main.c
@@ -94,6 +94,7 @@ unsigned int cursor_bin_len = 256;
 
 
 void main(void) {
+	bool frame = false;
 	x4_Begin();
 	x4_LoadDefaultPalette();
 	x4_FillScreen(15);
@@ -109,6 +110,9 @@ void main(void) {
 	lcd_CrsrXY = 0; // reset cursor position
 	lcd_CrsrClip = 0; // reset clipping
 	lcd_CrsrCtrl = 1; // enable cursor
+	int bg=15;
+	x4_SetDrawLocation(X4_BUFFER_2);
+	                x4_SetScreenLocation(X4_BUFFER_1);
 
 	while(!kb_IsDown(kb_KeyClear)) {
 		kb_Scan();
@@ -122,6 +126,24 @@ void main(void) {
 		if(y>=240)y=239;
 		lcd_CrsrY = x; // update the position of the cursor
 		lcd_CrsrX = y;
+		if(kb_IsDown(kb_KeyEnter)){
+			bg++;
+			if(bg>15)
+				bg=0;
+			x4_FillScreen(bg);
+			frame=!frame;
+			if(frame){
+				x4_SetDrawLocation(X4_BUFFER_1);
+				x4_SetScreenLocation(X4_BUFFER_2);
+			}else{
+				x4_SetDrawLocation(X4_BUFFER_2);
+				x4_SetScreenLocation(X4_BUFFER_1);
+			}
+			while(kb_IsDown(kb_KeyEnter)){
+				delay(10);
+				kb_Scan();
+			}
+		}
 	}
 
 	lcd_CrsrCtrl = 0; // disable cursor
